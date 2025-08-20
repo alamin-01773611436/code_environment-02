2f2a3a5c6f116880a100b5d5a635c7ac6600c763
Al-Amin <alamin@example.com>
2025-08-20 15:09:35 +0600

    It's [version - 0.1]

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-xl text-green-500 text-center bg-gray-500 p-3'>"We are here to learn React from tapaScript"</h1>
+    </div>
+  )
+}
+
+export default App
